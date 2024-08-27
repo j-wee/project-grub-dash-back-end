be53b48cb3dea258b21eb00163eb9ae34aae27fe
Joan Wee <jwee@indeed.com>
2024-08-27 15:31:45 +0900

    Add dish functionalities

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -10,8 +10,14 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
  */
 
+router.route("/:dishId")
+    .get(controller.read)
+    .put(controller.update)
+    .all(methodNotAllowed);
+
 router.route("/")
     .get(controller.list)
+    .post(controller.create)
     .all(methodNotAllowed);
 
 module.exports = router;
