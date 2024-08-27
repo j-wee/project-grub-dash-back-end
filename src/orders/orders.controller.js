0d7cc7533db48bc8e99e164a97afa27de37ead5b
Joan Wee <jwee@indeed.com>
2024-08-27 19:52:15 +0900

    Remove unnecessary comments and fix router config

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,12 +6,6 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
-
-/*
-    add handlers and middleware functions to create, read, update, delete, and list orders.
- */
-
 function list(req, res) {
     res.json({ data: orders });
 }
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,13 +1,6 @@
-const router = require("express").Router({ mergeParams: true });
+const router = require("express").Router();
 const controller = require("./orders.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-
-// TODO: Implement the /orders routes needed to make the tests pass
-
-/*
-    add two routes: /orders and /orders/:orderId. Attach the handlers (create, read, update, delete, and list)
-    exported from src/orders/orders.controller.js.
- */
 
 router.route("/:orderId")
     .get(controller.read)
